709c545a61be0b1b4287d9412638b0b0733371fa
victorduarte778@gmail.com <Victor-Drt>
2026-04-07 23:55:13 -0300

    feat: adiciona setup para banco de dados

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,3 +6,5 @@ load_dotenv(".env")
 SECRET_KEY = os.getenv("SECRET_KEY", "valor_default") 
 ALGORITHM = os.getenv("ALGORITHM", "HS256") 
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30) 
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "")
+
